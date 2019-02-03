66b34d63efadcc0fe868071b9d9dcb864ee19945
ahrbil <ahrbil.me@gmail.com>
2019-02-03 22:35:16 +0100

    adding server validation for inputs

---
+++ packages/server/src/input-validation/index.js
@@ -0,0 +1,18 @@
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(40, "too short for title")
+    .required("*Title is required"),
+  description: Yup.string()
+    .min(40, "too short for description")
+    .required("*Description is required"),
+  location: Yup.string()
+    .min(40, "too short for location address")
+    .required("*Location is required"),
+  tags: Yup.array()
+    .max(10, "Only 10 tags are allowed")
+    .required("Provide at least one tag")
+});
+
+export default validationSchema;
