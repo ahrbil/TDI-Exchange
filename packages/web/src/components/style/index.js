d817ef4227731ddde734f80d944879694a4a480f
ahrbil <ahrbil.me@gmail.com>
2019-02-06 23:25:05 +0100

    adding media query to mean css grid

--- packages/web/src/components/style/index.js
+++ packages/web/src/components/style/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { MEDIA_AT_A } from "../../constants";
+
 export const Container = styled.div`
   max-width: 1080px;
   margin: 0 auto;
@@ -9,21 +11,23 @@ export const Wrapper = styled.div`
   max-width: 1080px;
   min-height: calc(100vh - 120px);
   margin: 0 auto;
+  padding: 0px 16px;
+  @media (min-width: ${MEDIA_AT_A}) {
+    padding: 0px;
+  }
 `;
 export const HomeGrid = styled.div`
   display: grid;
-  grid-template-columns: 3fr minmax(240px, 1fr);
-  grid-template-areas: "main aside";
+  grid-template-columns: 1fr;
+  grid-template-areas: "main" "aside";
   grid-gap: 32px;
+  padding: 0 1rem;
   width: 100%;
   min-width: 100%;
   max-width: 100%;
   min-height: 100vh;
-`;
-
-export const AsideContainer = styled.div`
-  grid-area: aside;
-  align-items: stretch;
-  display: flex;
-  flex-direction: column;
+  @media (min-width: ${MEDIA_AT_A}) {
+    grid-template-columns: 3fr minmax(240px, 1fr);
+    grid-template-areas: "main aside";
+  }
 `;
