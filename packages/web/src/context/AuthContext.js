a9006c0c947726eff9e62ff2f8d9d0f628198342
ahrbil <ahrbil.me@gmail.com>
2019-02-07 17:09:35 +0100

    Updating me query to bypass the cache

--- packages/web/src/context/AuthContext.js
+++ packages/web/src/context/AuthContext.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Query } from "react-apollo";
+import PropTypes from "prop-types";
+
 import { CURRENT_USER } from "../queries";
 
 const AuthContext = React.createContext();
 
 export class AuthProvider extends React.PureComponent {
   render() {
+    const { children } = this.props;
     return (
-      <Query query={CURRENT_USER}>
+      <Query query={CURRENT_USER} fetchPolicy="no-cache">
         {({ data }) => (
           <AuthContext.Provider
             value={{ currentUser: data && data.me ? data.me : null }}
           >
-            {this.props.children}
+            {children}
           </AuthContext.Provider>
         )}
       </Query>
@@ -21,3 +24,7 @@ export class AuthProvider extends React.PureComponent {
 }
 
 export const AuthConsumer = AuthContext.Consumer;
+
+AuthProvider.propTypes = {
+  children: PropTypes.element.isRequired
+};
