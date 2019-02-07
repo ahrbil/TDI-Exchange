3d1fbafed4fa7d79460e54e8ecbcafca71beefdc
ahrbil <ahrbil.me@gmail.com>
2019-02-07 10:54:58 +0100

    pagination styled card

---
+++ packages/web/src/components/pagination/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.div`
+  padding: 1rem 0rem;
+`;
+
+export const PaginationCard = styled.div`
+  display: flex;
+  justify-content: center;
+  border-radius: 4.71px;
+  border: 1px solid #f2f2f2;
+  box-shadow: 1px 1px rgb(223, 225, 230);
+`;
