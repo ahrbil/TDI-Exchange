ddcc184d79ac76edf5cc99a7e4a03b55f721cfbe
ahrbil <ahrbil.me@gmail.com>
2019-02-07 17:08:06 +0100

    Adding apollo cache and load the app first on cache

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,64 +1,79 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { ApolloProvider } from "react-apollo";
-import { Router } from "@reach/router";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createUploadLink } from "apollo-upload-client";
+import { persistCache } from "apollo-cache-persist";
 
 import "./App.css";
 import theme from "./theme";
 import Header from "./components/header";
 import { Wrapper } from "./components/style";
 import { AuthProvider } from "./context/AuthContext";
-import {
-  Home,
-  QuestionDetails,
-  SignIn,
-  QuestionSearch,
-  Internships,
-  PostInternship
-} from "./pages";
+import Routes from "./routes";
 import Footer from "./components/footer";
-import CreateQuestion from "./components/create-question";
-import AuthRoute from "./components/AuthRoute";
 import { SearchProvider } from "./context/SearchContext";
+import Loader, { Wrapper as LoaderWrapper } from "./components/loader";
 
-const client = new ApolloClient({
-  // createUploadLink handles the file upload
-  link: createUploadLink({
-    uri: "http://localhost:4000/graphql",
-    credentials: "include"
-  }),
-  cache: new InMemoryCache()
-});
+class App extends React.Component {
+  state = {
+    loaded: false,
+    apolloClient: null
+  };
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <SearchProvider>
-          <Header />
-          <Wrapper>
-            <Router>
-              <Home path="/" />
-              <Internships path="/internships" />
-              <Internships path="/internships/tags/:tag" />
-              <QuestionDetails path="/questions/:qid" />
-              <QuestionSearch path="/search/results" />
-              <SignIn path="/sign-in" />
-              <AuthRoute path="/ask-a-question" render={<CreateQuestion />} />
-              <AuthRoute
-                path="/post-an-internship"
-                render={<PostInternship />}
-              />
-            </Router>
-          </Wrapper>
-          <Footer />
-        </SearchProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  </ApolloProvider>
-);
+  componentDidMount = async () => {
+    const cache = new InMemoryCache();
+    const apolloClient = new ApolloClient({
+      cache,
+      // createUploadLink handles the file upload
+      link: createUploadLink({
+        uri: "http://localhost:4000/graphql",
+        credentials: "include"
+      })
+    });
+
+    try {
+      // wait to persist cache from local storage
+      await persistCache({
+        cache,
+        storage: window.localStorage
+      });
+    } catch (error) {
+      console.error("Error restoring the cache");
+    }
+    // update the state with the client and set loaded to true
+    this.setState({
+      loaded: true,
+      apolloClient
+    });
+  };
+
+  render() {
+    const { apolloClient, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      );
+    }
+    return (
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <SearchProvider>
+              <Header />
+              <Wrapper>
+                <Routes />
+              </Wrapper>
+              <Footer />
+            </SearchProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    );
+  }
+}
 
 export default App;
