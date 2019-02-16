277d348b8bdbb1b84a6e634c4752445f51921987
ahrbil <ahrbil.me@gmail.com>
2019-02-16 22:51:49 +0100

    Updating cookie options

--- packages/server/src/middlewares/index.js
+++ packages/server/src/middlewares/index.js
@@ -2,13 +2,17 @@ import passport from "passport";
 import { Router } from "express";
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
+import { IS_PROD } from "../constants";
 
 const middleWare = Router();
 middleWare.use(
   cookieSession({
+    name: "tdi-cs",
     keys: [process.env.SESSION_SECRET],
     maxAge: 1000 * 60 * 60 * 24 * 7,
-  }),
+    domain: IS_PROD ? "tdi-exchange.now.sh" : "localhost",
+    httpOnly: IS_PROD
+  })
 );
 
 middleWare.use(passport.initialize());
