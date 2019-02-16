0136f9ca3c056fa136ca61378818f2f4965968f7
ahrbil <ahrbil.me@gmail.com>
2019-02-16 22:45:31 +0100

    Adding vars needed in for production

--- packages/server/src/constants.js
+++ packages/server/src/constants.js
@@ -7,3 +7,9 @@ export const DELETE_CREATE_ANSWER = -2;
 export const DELETE_CREATE_WORKING_ANSWER = -10;
 
 export const FILE_TYPES = ["image/gif", "image/png", "image/jpg", "image/jpeg"];
+
+export const PORT = process.env.PORT || 4000;
+export const IS_PROD = process.env.NODE_ENV === "production";
+export const HOST_URL = IS_PROD
+  ? "https://tdi-exchange.now.sh"
+  : "http://localhost:4000";
