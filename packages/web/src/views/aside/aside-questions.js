645956c2b61378e97f3eeee448e55ad75dfd6c17
ahrbil <ahrbil.me@gmail.com>
2019-02-26 14:55:26 +0100

    Add recent questions and internships aside component

---
+++ packages/web/src/views/aside/aside-questions.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { Link } from "@reach/router";
+
+import { QUESTIONS_FEED } from "../../queries";
+import { Container, AsideTitle, Wrapper } from "./style";
+import { TextButton } from "../../components/button";
+import Loader, { Wrapper as LoaderWrapper } from "../../components/loader";
+
+const AsideLatestQuestions = () => (
+  <Query
+    query={QUESTIONS_FEED}
+    variables={{
+      first: 6,
+      orderBy: "createdAt_DESC"
+    }}
+    fetchPolicy="cache-and-network"
+  >
+    {({ data, loading }) => (
+      <Wrapper>
+        <AsideTitle>Latest Questions</AsideTitle>
+        {loading && (
+          <LoaderWrapper>
+            <Loader />
+          </LoaderWrapper>
+        )}
+        {!loading && (
+          <>
+            {data.questionsFeed.items.map(item => (
+              <Container key={item.id}>
+                <Link to={`/questions/${item.id}`}>
+                  <p>{item.header}</p>
+                </Link>
+              </Container>
+            ))}
+            <Link to="/">
+              <TextButton small>View All</TextButton>
+            </Link>
+          </>
+        )}
+      </Wrapper>
+    )}
+  </Query>
+);
+
+export default AsideLatestQuestions;
