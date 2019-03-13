5867d0739b0ebc07c69214bd48bb0a55841b0bd1
ahrbil <ahrbil.me@gmail.com>
2019-03-13 23:09:09 +0100

    Add a function to check if a user have right permissions

--- packages/server/src/utils/index.js
+++ packages/server/src/utils/index.js
@@ -1,4 +1,6 @@
 import uploadImage from "./upload-images";
 import getUserEmail from "./getUserEmail";
+import isLoggedIn from "./is-logged-in";
+import isHavePermission from "./is-have-permission";
 
-export { uploadImage, getUserEmail };
+export { uploadImage, getUserEmail, isLoggedIn, isHavePermission };
---
+++ packages/server/src/utils/is-have-permission.js
@@ -0,0 +1,43 @@
+import { ForbiddenError } from "apollo-server-express";
+
+const QUESTION = "question";
+const ANSWER = "answer";
+
+const throwForbiddenError = () => {
+  throw new ForbiddenError(
+    `You don't have the right permissions for this action.`
+  );
+};
+
+const havePermissionQuestion = async (context, id) => {
+  const askedBy = await context.prisma.question({ id }).askedBy();
+  const currentUser = context.user;
+  if (askedBy.id === currentUser.id) {
+    return true;
+  }
+  throwForbiddenError();
+};
+const havePermissionAnswer = async (context, id) => {
+  const askedBy = await context.prisma.answer({ id }).askedBy();
+  const currentUser = context.user;
+  if (askedBy.id === currentUser.id) {
+    return true;
+  }
+  throwForbiddenError();
+};
+
+const isHavePermission = async (type, context, id) => {
+  switch (type) {
+    case QUESTION: {
+      const havePermission = await havePermissionQuestion(context, id);
+      return havePermission;
+    }
+    case ANSWER: {
+      const havePermission = await havePermissionAnswer(context, id);
+      return havePermission;
+    }
+    default:
+      break;
+  }
+};
+export default isHavePermission;
