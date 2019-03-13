35de1ad9373090ada0fb605aa3119882545a1888
ahrbil <ahrbil.me@gmail.com>
2019-03-13 17:07:28 +0100

    Remove apollo cache persist from app

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -10,51 +10,22 @@ import { AuthProvider } from "./context/AuthContext";
 import Routes from "./routes";
 import Footer from "./components/footer";
 import { SearchProvider } from "./context/SearchContext";
-import Loader, { Wrapper as LoaderWrapper } from "./components/loader";
-import { persistCache, apolloClient } from "./apolloClient";
+import { apolloClient } from "./apolloClient";
 
-class App extends React.Component {
-  state = {
-    loaded: false
-  };
-
-  componentDidMount = async () => {
-    try {
-      persistCache.restore().then(() => this.setState({ loaded: true }));
-    } catch (error) {
-      console.error("Error restoring the cache");
-    }
-    // update the state with the client and set loaded to true
-    this.setState({
-      loaded: true
-    });
-  };
-
-  render() {
-    const { loaded } = this.state;
-    if (!loaded) {
-      return (
-        <LoaderWrapper>
-          <Loader />
-        </LoaderWrapper>
-      );
-    }
-    return (
-      <ApolloProvider client={apolloClient}>
-        <ThemeProvider theme={theme}>
-          <AuthProvider>
-            <SearchProvider>
-              <Header />
-              <Wrapper>
-                <Routes />
-              </Wrapper>
-              <Footer />
-            </SearchProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </ApolloProvider>
-    );
-  }
-}
+const App = () => (
+  <ApolloProvider client={apolloClient}>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <SearchProvider>
+          <Header />
+          <Wrapper>
+            <Routes />
+          </Wrapper>
+          <Footer />
+        </SearchProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  </ApolloProvider>
+);
 
 export default App;
--- packages/web/src/apolloClient.js
+++ packages/web/src/apolloClient.js
@@ -1,19 +1,10 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createUploadLink } from "apollo-upload-client";
-import { CachePersistor } from "apollo-cache-persist";
 
 import { GQL_URI } from "./constants";
 
 const cache = new InMemoryCache();
-
-export const persistCache = new CachePersistor({
-  cache,
-  storage: window.localStorage,
-  key: "tdi",
-  maxSize: false,
-  debug: false
-});
 
 export const apolloClient = new ApolloClient({
   cache,
