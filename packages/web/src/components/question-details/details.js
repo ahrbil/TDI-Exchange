d0b0f22c20378b2494cb8177fa5ffb31fb77677d
ahrbil <ahrbil.me@gmail.com>
2019-03-15 12:58:00 +0100

    simplifying and composing two render props components

--- packages/web/src/components/question-details/details.js
+++ packages/web/src/components/question-details/details.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Mutation } from "react-apollo";
 import { navigate } from "@reach/router";
+import { adopt } from "react-adopt";
 
 import { QuestionBody } from "../question/style";
 import Answer from "../answer";
@@ -26,10 +27,23 @@ import EditQuestion from "./edit-answer";
 import { DELETE_QUESTION } from "../../queries";
 import Modal from "../modal";
 import Button, { TextButton } from "../button";
-import { formatError } from "../../utils";
+import DropDown, { DropDownItem } from "../drop-down";
+import { Text } from "../drop-down/style";
+// import { formatError } from "../../utils";
 
 const createdAtDESC = "createdAt_DESC";
 const createdAtASC = "createdAt_ASC";
+
+const QuestionMutation = ({ render }) => (
+  <Mutation mutation={DELETE_QUESTION}>
+    {(deleteQuestion, result) => render({ deleteQuestion, result })}
+  </Mutation>
+);
+
+const Composed = adopt({
+  mutation: QuestionMutation,
+  currentUser: <AuthConsumer />
+});
 
 const Details = ({
   question,
@@ -43,16 +57,11 @@ const Details = ({
   setIsEditing
 }) => {
   const { isOwner } = question;
-  const [dropdownBtnHover, setDropdownBtnHover] = React.useState(false);
   const [modalOpen, setModalOpen] = React.useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownBtnHover(!dropdownBtnHover);
-  };
 
   const deleteQuestionAndRedirect = async deleteQuestion => {
     try {
-      await deleteQuestion();
+      await deleteQuestion({ variables: { questionId: question.id } });
       await navigate("/");
     } catch (err) {
       if (err.graphQLErrors) {
@@ -62,146 +71,135 @@ const Details = ({
     }
   };
 
+  const Menu = () => (
+    <Fragment>
+      <DropDownItem onClick={() => setIsEditing(true)} icon="edit">
+        <Text>Edit</Text>
+      </DropDownItem>
+      <DropDownItem
+        className="danger"
+        onClick={() => setModalOpen(true)}
+        icon="delete"
+      >
+        <Text>Delete</Text>
+      </DropDownItem>
+    </Fragment>
+  );
+
   return (
-    <AuthConsumer>
-      {({ currentUser }) => (
-        <Mutation
-          mutation={DELETE_QUESTION}
-          variables={{ questionId: question.id }}
-        >
-          {(deleteQuestion, mutationResult) => {
-            const deleting = mutationResult.loading;
-            return isEditing ? (
-              <EditQuestion
-                isEditing={isEditing}
-                setIsEditing={setIsEditing}
-                header={question.header}
-                body={question.body}
-                questionId={question.id}
-              />
-            ) : (
-              <DetailsContainerStyle>
-                {/* question header style */}
-                <QuestionHeader
-                  user={question.askedBy}
-                  createdAt={question.createdAt}
-                />
-                {/* question header */}
-                <QuestionBody>
-                  <h1>{question.header}</h1>
-                </QuestionBody>
-                {/* question body if it is exists */}
-                {question.body && (
-                  <QuestionDetails>
-                    <RichRender body={question.body} />
-                  </QuestionDetails>
-                )}
-                {/* total answer a question have */}
-                <ActionBarStyle>
-                  <h3>{`${question.totalAnswers} Answers`}</h3>
-                  <div>
-                    <OutLinedBtn
-                      onClick={() => handleOrderBy(createdAtDESC)}
-                      activeDesc={activeDesc}
+    <Composed>
+      {({ mutation: { deleteQuestion, result }, currentUser }) => {
+        const deleting = result.loading;
+        return isEditing ? (
+          <EditQuestion
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+            header={question.header}
+            body={question.body}
+            questionId={question.id}
+          />
+        ) : (
+          <DetailsContainerStyle>
+            {/* question header style */}
+            <QuestionHeader
+              user={question.askedBy}
+              createdAt={question.createdAt}
+            />
+            {/* question header */}
+            <QuestionBody>
+              <h1>{question.header}</h1>
+            </QuestionBody>
+            {/* question body if it is exists */}
+            {question.body && (
+              <QuestionDetails>
+                <RichRender body={question.body} />
+              </QuestionDetails>
+            )}
+            {/* total answer a question have */}
+            <ActionBarStyle>
+              <h3>{`${question.totalAnswers} Answers`}</h3>
+              <div>
+                <OutLinedBtn
+                  onClick={() => handleOrderBy(createdAtDESC)}
+                  activeDesc={activeDesc}
+                >
+                  <Icon iconName="arrowUp" />
+                </OutLinedBtn>
+                <OutLinedBtn
+                  onClick={() => handleOrderBy(createdAtASC)}
+                  activeAsc={activeAsc}
+                >
+                  <Icon iconName="arrowDown" />
+                </OutLinedBtn>
+                {currentUser && isOwner && (
+                  <Fragment>
+                    <DropDown overlay={<Menu />}>
+                      <OutLinedBtn>
+                        <Icon iconName="gear" />
+                      </OutLinedBtn>
+                    </DropDown>
+                    <Modal
+                      title="Are you sure you want to delete this question?"
+                      isOpen={modalOpen}
+                      closeModal={() => !deleting && setModalOpen(false)}
                     >
-                      <Icon iconName="arrowUp" />
-                    </OutLinedBtn>
-                    <OutLinedBtn
-                      onClick={() => handleOrderBy(createdAtASC)}
-                      activeAsc={activeAsc}
-                    >
-                      <Icon iconName="arrowDown" />
-                    </OutLinedBtn>
-                    {currentUser && isOwner && (
-                      <div
-                        onMouseEnter={toggleDropdown}
-                        onMouseLeave={toggleDropdown}
-                        style={{ position: "relative" }}
-                      >
-                        <OutLinedBtn>
-                          <Icon iconName="gear" />
-                        </OutLinedBtn>
-                        {dropdownBtnHover && (
-                          <DropDownContent>
-                            <ul>
-                              <DropDownItem onClick={() => setIsEditing(true)}>
-                                <Icon iconName="edit" />
-                                <Text>Edit</Text>
-                              </DropDownItem>
-                              <DropDownItem
-                                className="danger"
-                                onClick={() => setModalOpen(true)}
-                              >
-                                <Icon iconName="delete" />
-                                <Text>Delete</Text>
-                              </DropDownItem>
-                            </ul>
-                          </DropDownContent>
-                        )}
-                        <Modal
-                          title="Are you sure you want to delete this question?"
-                          isOpen={modalOpen}
-                          closeModal={() => !deleting && setModalOpen(false)}
+                      <ModalBtns>
+                        <Button
+                          onClick={() =>
+                            deleteQuestionAndRedirect(deleteQuestion)
+                          }
+                          loading={deleting}
                         >
-                          <ModalBtns>
-                            <Button
-                              onClick={() =>
-                                deleteQuestionAndRedirect(deleteQuestion)
-                              }
-                              loading={deleting}
-                            >
-                              Delete
-                            </Button>
-                            {!deleting && (
-                              <TextButton onClick={() => setModalOpen(false)}>
-                                Cancel
-                              </TextButton>
-                            )}
-                          </ModalBtns>
-                        </Modal>
-                      </div>
-                    )}
-                  </div>
-                </ActionBarStyle>
-                {/* answer section */}
-                {loading ? (
-                  <AnswerContentLoader />
-                ) : (
-                  <AnswersSection>
-                    {question.answers.map(answer => (
-                      <Answer key={answer.id} answer={answer} />
-                    ))}
-                  </AnswersSection>
+                          Delete
+                        </Button>
+                        {!deleting && (
+                          <TextButton onClick={() => setModalOpen(false)}>
+                            Cancel
+                          </TextButton>
+                        )}
+                      </ModalBtns>
+                    </Modal>
+                  </Fragment>
                 )}
-                {/* pagination */}
-                {question.totalAnswers > ITEMS_ON_PAGE && !loading && (
-                  <Pagination
-                    defaultCurrent={1}
-                    total={question.totalAnswers}
-                    current={currentPage}
-                    pageSize={ITEMS_ON_PAGE}
-                    onChange={handlePaginationChange}
-                    style={{ alignSelf: "center", margin: "2rem 0 0.5rem 0" }}
-                  />
-                )}
-                {currentUser && (
-                  <ActionBarStyle>
-                    <span>Contribute to this question</span>
-                  </ActionBarStyle>
-                )}
-                {/* create answer input */}
-                {currentUser && <CreateAnswer questionId={question.id} />}
-                {!currentUser && (
-                  <ActionBarStyle>
-                    <LogInModal />
-                  </ActionBarStyle>
-                )}
-              </DetailsContainerStyle>
-            );
-          }}
-        </Mutation>
-      )}
-    </AuthConsumer>
+              </div>
+            </ActionBarStyle>
+            {/* answer section */}
+            {loading ? (
+              <AnswerContentLoader />
+            ) : (
+              <AnswersSection>
+                {question.answers.map(answer => (
+                  <Answer key={answer.id} answer={answer} />
+                ))}
+              </AnswersSection>
+            )}
+            {/* pagination */}
+            {question.totalAnswers > ITEMS_ON_PAGE && !loading && (
+              <Pagination
+                defaultCurrent={1}
+                total={question.totalAnswers}
+                current={currentPage}
+                pageSize={ITEMS_ON_PAGE}
+                onChange={handlePaginationChange}
+                style={{ alignSelf: "center", margin: "2rem 0 0.5rem 0" }}
+              />
+            )}
+            {currentUser && (
+              <ActionBarStyle>
+                <span>Contribute to this question</span>
+              </ActionBarStyle>
+            )}
+            {/* create answer input */}
+            {currentUser && <CreateAnswer questionId={question.id} />}
+            {!currentUser && (
+              <ActionBarStyle>
+                <LogInModal />
+              </ActionBarStyle>
+            )}
+          </DetailsContainerStyle>
+        );
+      }}
+    </Composed>
   );
 };
 
@@ -229,45 +227,6 @@ Details.propTypes = {
   setIsEditing: PropTypes.func.isRequired
 };
 
-const DropDownContent = styled.div`
-  position: absolute;
-  right: 0;
-  border: 1px solid #f8f8f8;
-  padding: 8px 0px;
-  background-color: white;
-  box-shadow: 0px 0px 18px 0px #d7d4d4;
-  border-radius: 4.71px;
-  width: 170px;
-  z-index: 100;
-  > ul {
-    list-style: none;
-  }
-  .danger {
-    color: ${props => props.theme.error.primary};
-  }
-`;
-const DropDownItem = styled.li`
-  padding: 8px 16px;
-  margin: 0.1rem 0rem;
-  border-bottom: 1px solid #e6e4e4;
-  transition: all 0.2s ease-in;
-  cursor: pointer;
-  display: flex;
-  &:last-of-type {
-    border-bottom: none;
-  }
-  &:hover {
-    background-color: rgb(247, 249, 250);
-  }
-  > div {
-    width: 21px;
-    margin-right: 0.6rem;
-  }
-`;
-const Text = styled.span`
-  font-size: 0.9rem;
-  font-weight: 500;
-`;
 const ModalBtns = styled.div`
   display: flex;
   > button {
