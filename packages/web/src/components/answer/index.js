f0ccb5dc086ea7998a7f7f2ef850dc392847fed0
_ ahrbil <ahrbil.me@gmail.com>
2019-03-17 22:22:30 +0100

    implementing delete answer in front-end

--- packages/web/src/components/answer/index.js
+++ packages/web/src/components/answer/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 
+import { Mutation } from "react-apollo";
 import RichRender from "../rich-texte-rendrer";
 import QuestionHeader from "../question-details/questionHeader";
 import Icon from "../icons";
@@ -8,50 +9,96 @@ import DropDown, { DropDownItem } from "../drop-down";
 import { Text, DropDownWrapper } from "../drop-down/style";
 import { OutLinedBtn } from "../question-details/style";
 import CreateAnswer from "../create-answer";
+import { DELETE_ANSWER, QUESTION_WITH_DETAILS } from "../../queries";
+import Button, { TextButton } from "../button";
+import Modal from "../modal";
 
 const Answer = ({ answer, questionId, currentUser }) => {
   const [isEdit, setIsEdit] = React.useState(false);
+  const [modalOpen, setModalOpen] = React.useState(false);
   const toggleEdit = () => {
     setIsEdit(!isEdit);
   };
 
-  const Menu = () => (
+  const Menu = ({ openModal }) => (
     <Fragment>
       <DropDownItem icon="edit" onClick={toggleEdit}>
         <Text>Edit</Text>
       </DropDownItem>
-      <DropDownItem icon="delete" className="danger">
+      <DropDownItem
+        icon="delete"
+        className="danger"
+        onClick={() => openModal(true)}
+      >
         <Text>Delete</Text>
       </DropDownItem>
     </Fragment>
   );
   return (
-    <AnswerStyle>
-      <QuestionHeader
-        user={answer.answeredBy}
-        createdAt={answer.createdAt}
-        forAnswer
-      />
-      {isEdit ? (
-        <CreateAnswer
-          answerId={answer.id}
-          questionId={questionId}
-          isEdit={isEdit}
-          rawContent={answer.body}
-          toggleEdit={toggleEdit}
-        />
-      ) : (
-        <RichRender body={answer.body} />
-      )}
+    <Mutation
+      mutation={DELETE_ANSWER}
+      variables={{ answerId: answer.id }}
+      refetchQueries={[
+        { query: QUESTION_WITH_DETAILS, variables: { id: questionId } }
+      ]}
+      onCompleted={() => setModalOpen(false)}
+    >
+      {(deleteAnswer, { loading }) => (
+        <AnswerStyle>
+          <QuestionHeader
+            user={answer.answeredBy}
+            createdAt={answer.createdAt}
+            forAnswer
+          />
+          {isEdit ? (
+            <CreateAnswer
+              answerId={answer.id}
+              questionId={questionId}
+              isEdit={isEdit}
+              rawContent={answer.body}
+              toggleEdit={toggleEdit}
+            />
+          ) : (
+            <RichRender body={answer.body} />
+          )}
 
-      {currentUser && answer.isOwner && (
-        <DropDown overlay={<Menu />}>
-          <OutLinedBtn className="dropIcon">
-            <Icon iconName="squareDots" />
-          </OutLinedBtn>
-        </DropDown>
+          {currentUser && answer.isOwner && (
+            <Fragment>
+              <DropDown overlay={<Menu openModal={setModalOpen} />}>
+                <OutLinedBtn className="dropIcon">
+                  <Icon iconName="squareDots" />
+                </OutLinedBtn>
+              </DropDown>
+              <Modal
+                title="Are you sure you want to delete this answer?"
+                isOpen={modalOpen}
+                closeModal={() => !loading && setModalOpen(false)}
+              >
+                <ModalBtns>
+                  <Button
+                    onClick={async () => {
+                      try {
+                        await deleteAnswer();
+                      } catch (error) {
+                        console.log(error);
+                      }
+                    }}
+                    loading={loading}
+                  >
+                    Delete
+                  </Button>
+                  {!loading && (
+                    <TextButton onClick={() => setModalOpen(false)}>
+                      Cancel
+                    </TextButton>
+                  )}
+                </ModalBtns>
+              </Modal>
+            </Fragment>
+          )}
+        </AnswerStyle>
       )}
-    </AnswerStyle>
+    </Mutation>
   );
 };
 
@@ -76,5 +123,12 @@ const AnswerStyle = styled.div`
   }
   .markdown {
     padding-top: 0;
+  }
+`;
+
+const ModalBtns = styled.div`
+  display: flex;
+  > button {
+    margin-right: 1rem;
   }
 `;
