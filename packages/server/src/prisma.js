f40f9bd452c1677bd79eb4eccbac28f8cf13423a
ahrbil <ahrbil.me@gmail.com>
2019-04-13 23:25:24 +0100

    Update prisma endpoint

--- packages/server/src/prisma.js
+++ packages/server/src/prisma.js
@@ -1,8 +1,22 @@
 import { Prisma } from "./generated/prisma-client";
+import { IS_PROD } from "./constants";
+
+// db endpoint
+const endpoint = IS_PROD
+  ? process.env.PRISMA_ENDPOINT
+  : process.env.PRISMA_ENDPOINT_DEV;
+
+// db secret
+const secret = IS_PROD
+  ? process.env.PRISMA_SECRET
+  : process.env.PRISMA_SECRET_DEV;
 
 const getPrisma = () => {
   try {
-    const db = new Prisma();
+    const db = new Prisma({
+      endpoint,
+      secret
+    });
     return db;
   } catch (err) {
     // console.log("prisma err", err);
