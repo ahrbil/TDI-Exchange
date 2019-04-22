26da84a1ad470e1356914f05dc708163c170a8a9
ahrbil <ahrbil.me@gmail.com>
2019-04-22 21:47:14 +0100

    Update user profile to use drop down component

--- packages/web/src/components/profile/index.js
+++ packages/web/src/components/profile/index.js
@@ -1,94 +1,47 @@
-import React from "react";
-import styled from "styled-components";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { Link } from "@reach/router";
 
-import Icon from "../icons";
+import DropDown, { DropDownItem } from "../drop-down";
+import { Text } from "../drop-down/style";
+import {
+  Avatar,
+  UserAvatar,
+  UserName,
+  ProfileStyle,
+  ProfileInfo
+} from "./style";
 
-const Profile = ({ user }) => (
-  <ProfileStyle>
-    <img src={user.avatar} alt={user.userName} />
-    <DropDown>
-      <DropDownItem>
-        <h1>{user.userName}</h1>
-      </DropDownItem>
+const Profile = ({ user }) => {
+  const Menu = () => (
+    <Fragment>
+      <ProfileInfo>
+        <Avatar>
+          <img src={user.avatar} alt={user.userName} />
+        </Avatar>
+        <div className="full-Width">
+          <UserName>{user.userName}</UserName>
+        </div>
+      </ProfileInfo>
       <Link to="logout">
         <DropDownItem>
-          <Icon
-            iconName="logout"
-            style={{ marginRight: "12px", width: "21px" }}
-          />
-          logout
+          <Text>Logout</Text>
         </DropDownItem>
       </Link>
-    </DropDown>
-  </ProfileStyle>
-);
+    </Fragment>
+  );
+  return (
+    <ProfileStyle>
+      <DropDown overlay={<Menu />}>
+        <UserAvatar>
+          <img src={user.avatar} alt={user.userName} />
+        </UserAvatar>
+      </DropDown>
+    </ProfileStyle>
+  );
+};
 
 export default Profile;
-
-const ProfileStyle = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
-  cursor: pointer;
-  border-radius: 100%;
-  transition: all 0.2s ease-in;
-  border: 2px solid transparent;
-  &:hover,
-  &:active {
-    background-color: #f8f8f8;
-    border-color: ${props => props.theme.color.primary};
-    > div {
-      visibility: visible;
-    }
-  }
-  h1 {
-    font-size: 0.8rem;
-    font-weight: 700;
-    margin-right: 11px;
-  }
-  img {
-    width: 31px;
-    height: 31px;
-    border-radius: 100%;
-  }
-`;
-
-const DropDown = styled.div`
-  display: flex;
-  visibility: hidden;
-  position: absolute;
-  right: 5px;
-  top: 45px;
-  background-color: white;
-  box-shadow: 3px 3px 18px 0px #d3d3d9;
-  flex-direction: column;
-  &:after {
-    content: "";
-    position: absolute;
-    bottom: 100%;
-    left: 90%;
-    margin-left: -10px;
-    border-width: 10px;
-    border-style: solid;
-    border-color: transparent transparent white transparent;
-  }
-`;
-
-const DropDownItem = styled.span`
-  font-size: 1rem;
-  padding: 10px 20px;
-  font-weight: 600;
-  text-transform: capitalize;
-  display: flex;
-  align-items: center;
-  border-bottom: 3px solid transparent;
-  transition: border-color 0.2s ease-in;
-  &:hover {
-    border-bottom: 3px solid ${props => props.theme.color.primary};
-  }
-`;
 
 Profile.propTypes = {
   user: PropTypes.shape({
---
+++ packages/web/src/components/profile/style.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+import { DropDownContent, DropDownItemStyle } from "../drop-down/style";
+
+export const Avatar = styled.div`
+  width: 45px;
+  min-width: 45px;
+  height: 45px;
+  background-color: aliceblue;
+  border-radius: 100%;
+  overflow: hidden;
+  margin-right: 1rem;
+  box-shadow: 0px 3px 9px #0000004a;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    user-select: none;
+  }
+`;
+
+export const UserAvatar = styled.button`
+  width: 32px;
+  height: 32px;
+  background: none;
+  display: block;
+  cursor: pointer;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 100%;
+  }
+`;
+
+export const UserName = styled.h1`
+  font-weight: bold;
+  font-size: 0.8rem;
+`;
+
+export const ProfileStyle = styled.div`
+  ${DropDownContent} {
+    padding-top: 0;
+    width: 250px;
+  }
+  .full-Width {
+    flex: 1;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
+
+export const ProfileInfo = styled(DropDownItemStyle)`
+  background-color: rgb(247, 249, 250);
+  cursor: auto;
+  padding: 1rem 16px;
+  margin-bottom: 0.4rem;
+  flex-direction: row;
+  align-items: center;
+  border: none;
+`;
