af7dfff6ef49e009b00005238a71c6e4a742ee65
ahrbil <ahrbil.me@gmail.com>
2019-04-30 20:32:12 +0100

    exporting cookie session

--- packages/server/src/middlewares/index.js
+++ packages/server/src/middlewares/index.js
@@ -4,16 +4,16 @@ import cookieSession from "cookie-session";
 import { IS_PROD } from "../constants";
 
 const middleWare = Router();
-middleWare.use(
-  cookieSession({
-    name: "tdi-cs",
-    keys: [process.env.SESSION_SECRET],
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    domain: IS_PROD ? "tdi-exchange.now.sh" : "localhost",
-    httpOnly: IS_PROD
-  })
-);
 
+export const session = cookieSession({
+  name: "tdi-cs",
+  keys: [process.env.SESSION_SECRET],
+  maxAge: 1000 * 60 * 60 * 24 * 7,
+  domain: IS_PROD ? "tdi-exchange.now.sh" : "localhost",
+  httpOnly: IS_PROD
+});
+
+middleWare.use(session);
 middleWare.use(passport.initialize());
 middleWare.use(passport.session());
 
